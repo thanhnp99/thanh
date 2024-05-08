b9873bfdb63c7539fd14796c14e81a5caffe8aba
thanhnp99 <nguyenphuongthanh.12a2@gmail.com>
2024-05-08 11:00:42 +0700

    add nghich

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+package First;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("nghien con ngai");
+		System.out.println("choiw ddas;jd;pwqoik ");
+		System.out.println("choiw ddas;jd;pwqoik ");
+		System.out.println("choiw ddas;jd;pwqoik ");
+		System.out.println("choiw ddas;jd;pwqoik ");
+		System.out.println("choiw ddas;jd;pwqoik ");
+
+	}
+}
